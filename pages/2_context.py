a5ae8be866445e91ca10b0882773d41b4ef9d4f0
Chinmay998 <122522457+Chinmay998@users.noreply.github.com>
2023-08-16 16:28:53 +0530

    Update 2_context.py

--- pages/2_context.py
+++ pages/2_context.py
@@ -3,6 +3,7 @@ from snowflake.snowpark import Session
 from snowflake.snowpark.functions import *
 import pandas as pd
 import streamlit as st
+st.write('Hello')
 def role_selection(_session):
     role_df = _session.sql('show roles;').collect()
     role_df = pd.DataFrame(role_df)
@@ -20,4 +21,14 @@ def warehouse_selection(_session):
     warehouse_select = st.sidebar.selectbox('Select a Role', warehouse_list)
     if st.sidebar.button('Use Warehouse'):
         set_warehouse = _session.sql(f'''USE WAREHOUSE {warehouse_select} ;''').collect()
+try:
+    session = st.session_state['Session']
+    set_role = role_selection(session)
+    if set_role != '':
+        warehouse_selection(session)
+    
+    current_role = session.sql('select current_role();').collect()
+    current_role
+    current_warehouse = session.sql('select current_warehouse();').collect()
+    current_warehouse
 
