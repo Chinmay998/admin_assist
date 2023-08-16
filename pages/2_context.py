95afc707c339007e367f2fa1c5f266b1d567fdcf
Chinmay998 <122522457+Chinmay998@users.noreply.github.com>
2023-08-16 16:20:07 +0530

    Update 2_context.py

--- pages/2_context.py
+++ pages/2_context.py
@@ -3,19 +3,21 @@ from snowflake.snowpark import Session
 from snowflake.snowpark.functions import *
 import pandas as pd
 import streamlit as st
-def wh_list(session):
-    warehouse_list_df = session.sql("show warehouses ;").collect()
-    warehouse_list_df =  pd.DataFrame(warehouse_list_df)
-    warehouse_list_df = warehouse_list_df["name"]
-    return warehouse_list_df
+def role_selection(_session):
+    role_df = _session.sql('show roles;').collect()
+    role_df = pd.DataFrame(role_df)
+    role_list = role_df['name']
+    role_select = st.sidebar.selectbox('Select a Role', role_list)
+    if st.sidebar.button('Use Role'):
+        set_role = _session.sql(f'''USE ROLE {role_select} ;''').collect()
+        return set_role
 
-
-def data_selector():
-    session = st.session_state['Session']
-    warehouse = wh_list(session)
-    wh_select = st.selectbox('Choose warehouse',(warehouse))
-    tables = tables_list(wh_select, session)
-    if list(tables) != []:
-        table_select = st.selectbox('Choose table',(tables))
-        return session, db_select,table_select
+def warehouse_selection(_session):
+    warehouse_df = _session.sql('show warehouses;').collect()
+    warehouse_df = pd.DataFrame(warehouse_df)
+    warehouse_df
+    warehouse_list = warehouse_df['name']
+    warehouse_select = st.sidebar.selectbox('Select a Role', warehouse_list)
+    if st.sidebar.button('Use Warehouse'):
+        set_warehouse = _session.sql(f'''USE WAREHOUSE {warehouse_select} ;''').collect()
 
