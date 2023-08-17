53cda18ed861db0ef4c921937c14984acb365b7d
Chinmay998 <122522457+Chinmay998@users.noreply.github.com>
2023-08-17 22:46:13 +0530

    Update 2_context.py

--- pages/2_context.py
+++ pages/2_context.py
@@ -5,7 +5,7 @@ import pandas as pd
 import streamlit as st
 st.title('User Management')
 def role_selection(session):
-    role_df = _session.sql('show roles;').collect()
+    role_df = session.sql('show roles;').collect()
     role_df = pd.DataFrame(role_df)
     role_list = role_df['name']
     role_select = st.sidebar.selectbox('Select a Role', role_list)
@@ -15,13 +15,13 @@ def role_selection(session):
         
 
 def warehouse_selection(session):
-    warehouse_df = _session.sql('show warehouses;').collect()
+    warehouse_df = session.sql('show warehouses;').collect()
     warehouse_df = pd.DataFrame(warehouse_df)
     
     warehouse_list = warehouse_df['name']
     warehouse_select = st.sidebar.selectbox('Select a Role', warehouse_list)
     if st.sidebar.button('Use Warehouse'):
-        set_warehouse = _session.sql(f'''USE WAREHOUSE {warehouse_select} ;''').collect()
+        set_warehouse = session.sql(f'''USE WAREHOUSE {warehouse_select} ;''').collect()
         st.session_state.selected_warehouse = warehouse_select
 
 
