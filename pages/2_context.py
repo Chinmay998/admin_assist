7c80c33fb1bf89db4675181f2aa23b7056562ad5
Chinmay998 <122522457+Chinmay998@users.noreply.github.com>
2023-08-18 10:01:04 +0530

    aligned indented block

--- pages/2_context.py
+++ pages/2_context.py
@@ -25,10 +25,10 @@ def warehouse_selection(session):
         set_warehouse = session.sql(f'''USE WAREHOUSE {warehouse_select} ;''').collect()
         st.session_state.selected_warehouse = warehouse_select
     
-except exception as e:
-st.sidebar.error(f"Error fetching warehouse: {e})
-warehouse_list = [] #clear the warehouse list
-warehouse_select = st.sidebar.selectbox('Select a Warehouse', warehouse_list)
+    except exception as e:
+    st.sidebar.error(f"Error fetching warehouse: {e})
+    warehouse_list = [] #clear the warehouse list
+    warehouse_select = st.sidebar.selectbox('Select a Warehouse', warehouse_list)
 
 
 
