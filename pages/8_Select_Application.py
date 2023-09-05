5abbec49b2bbf025c71025a98efcab2fc68372f1
Chinmay998 <122522457+Chinmay998@users.noreply.github.com>
2023-09-05 11:20:02 +0530

    Update 8_Select_Application.py

--- pages/8_Select_Application.py
+++ pages/8_Select_Application.py
@@ -3,7 +3,7 @@ import streamlit as st
 apps = {
     "App 1": "https://github.com/Chinmay998/admin_assist/raw/main/pages/2_Context.py",
     "App 2": "https://raw.githubusercontent.com/Chinmay998/admin_assist/main/pages/4_Over_Under_Size_File_Identifier.py",
-    "App 3": "URL_TO_APP_3_RAW",
+    "App 3": "https://raw.githubusercontent.com/Chinmay998/admin_assist/main/pages/3_File_Upload_Utility.py",
     "App 4": "URL_TO_APP_4_RAW",
     "App 5": "URL_TO_APP_5_RAW",
 }
